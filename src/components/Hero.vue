dc3c2f57690a2d659133851e5ef42ba9401e3563
Resul Elezi <resul_elezi@protonmail.com>
2024-10-03 15:13:37 +0200

    #

--- src/components/Hero.vue
+++ src/components/Hero.vue
@@ -1,5 +1,18 @@
+<script setup>
+import icon from '@/assets/img/icon-moon.svg'
+</script>
 
-<template>
-    <h1>Todo</h1>
-    <img src="/src/assets/img/icon-moon.svg" alt="">
-</template>
+<template class="hero">
+    <div class="bg">
+      <h1>Todo</h1>
+      <img :src="icon" alt="Moon Icon">
+    </div>
+  </template>
+  
+  <style scoped>
+  .bg {
+    background-image: url('@/assets/img/bg-desktop-dark.jpg');
+    background-size: cover;
+    background-position: center;
+  }
+  </style>
