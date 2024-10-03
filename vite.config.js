75352919a0adffeae1c7071258b921e917d06fc4
Resul Elezi <resul_elezi@protonmail.com>
2024-10-03 16:55:10 +0200

    #

--- vite.config.js
+++ vite.config.js
@@ -5,7 +5,7 @@ import vue from '@vitejs/plugin-vue'
 
 // https://vitejs.dev/config/
 export default defineConfig({
-  base: "/todo-app-fem/",
+  base: "https://resul-elezi.github.io/todo-app-fem/",
   plugins: [
     vue(),
   ],
