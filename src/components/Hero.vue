614e694e88f06d96da0e3320598a441ff35781d2
Resul Elezi <resul_elezi@protonmail.com>
2024-10-04 10:31:43 +0200

    #

--- src/components/Hero.vue
+++ src/components/Hero.vue
@@ -1,19 +1,19 @@
 <script setup>
-import iconMoon from '@/assets/img/icon-moon.svg'
 import iconSun from '@/assets/img/icon-sun.svg'
+import iconMoon from '@/assets/img/icon-moon.svg'
 </script>
 
 <template class="hero">
     <div class="bg">
       <h1>Todo</h1>
-      <img :src="iconMoon" alt="Moon Icon">
       <img :src="iconSun" alt="Sun Icon">
+      <img :src="iconMoon" alt="Moon Icon">
     </div>
   </template>
   
   <style scoped>
   .bg {
-    background-image: url('@/assets/img/bg-desktop-dark.jpg');
+    background-image: url('@/assets/img/bg-desktop-light.jpg');
     background-size: cover;
     background-position: center;
   }
