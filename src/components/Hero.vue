c7eb01c42632f35eeb9c1b0b74b9f248cc49b307
Resul Elezi <resul_elezi@protonmail.com>
2025-03-17 20:59:55 +0100

    Update Hero.vue

--- src/components/Hero.vue
+++ src/components/Hero.vue
@@ -1,17 +1,14 @@
 <script setup>
 import iconMoon from '@/assets/img/icon-moon.svg'
 import iconSun from '@/assets/img/icon-sun.svg'
-
 import { ref } from 'vue';
 
 const isDarkMode = ref(document.documentElement.getAttribute('data-theme') === 'dark');
-
 function toggleTheme() {
   isDarkMode.value = !isDarkMode.value;
   document.documentElement.setAttribute('data-theme', isDarkMode.value ? 'dark' : 'light');
 }
 </script>
-
 <template class="hero">
     <div class="bg">
       <h1>Todo</h1>
@@ -22,7 +19,6 @@ function toggleTheme() {
       <img :src="iconSun" alt="Sun Icon"> -->
     </div>
   </template>
-  
   <style scoped>
   .bg {
     background-image: url('@/assets/img/bg-desktop-light.jpg');
